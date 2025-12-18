cc652fa7ef18d6143649777b4afb292e84d0fa0e
Angelo Dylan <angelo.quispe.l@tecsup.edu.pe>
2025-12-18 01:02:47 -0500

    Feat: Crear array de botones de la calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -3,6 +3,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -28,6 +29,29 @@ public class Calculadora {
     // Creando panel donde iran los botones de la Calculadora
     JPanel panel = new JPanel();
     panel.setLayout(new GridLayout(5, 4, 5, 5));
+
+    // Creando array de botones para la Calculadora
+    JButton[] arrayButtons = new JButton[20];
+    arrayButtons[0] = new JButton("AC");
+    arrayButtons[1] = new JButton("C");
+    arrayButtons[2] = new JButton("√)");
+    arrayButtons[3] = new JButton("%");
+    arrayButtons[4] = new JButton("7");
+    arrayButtons[5] = new JButton("8");
+    arrayButtons[6] = new JButton("9");
+    arrayButtons[7] = new JButton("÷");
+    arrayButtons[8] = new JButton("4");
+    arrayButtons[9] = new JButton("5");
+    arrayButtons[10] = new JButton("6");
+    arrayButtons[11] = new JButton("x");
+    arrayButtons[12] = new JButton("1");
+    arrayButtons[13] = new JButton("2");
+    arrayButtons[14] = new JButton("3");
+    arrayButtons[15] = new JButton("-");
+    arrayButtons[16] = new JButton("0");
+    arrayButtons[17] = new JButton(".");
+    arrayButtons[18] = new JButton("=");
+    arrayButtons[19] = new JButton("+");
 
     // Añadiendo elementos a la ventana principal
     ventana.add(panel, BorderLayout.SOUTH);
